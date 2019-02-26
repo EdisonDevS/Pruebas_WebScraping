3467713996a2c375b61ecc5b5167679e58cb93f6
Jhon Edison Giraldo <jhonedisongiraldo99@gmail.com>
2019-02-26 16:45:37 -0500

    arreglitos

--- client.py
+++ client.py
@@ -6,15 +6,14 @@ import socket
 sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 config=("192.168.0.17",10000)
-print("Conectndo con servidor")
-sock.connect(config)
-print("conexion establecida")
-
-contacto=input("Contacto: ")
-
-sock.send(contacto.encode('utf-8'))
 
 while True:
+    contacto=input("Contacto: ")
+    send_msg(contacto)
+
+def send_msg(contacto):
+    sock.connect(config)
+    print("conexion establecida")
+    sock.send(contacto.encode('utf-8'))
     msg=input("Mensaje: ")
     sock.send(msg.encode('utf-8'))
-    sock.close()
