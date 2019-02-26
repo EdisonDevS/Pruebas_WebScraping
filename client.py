a251a51f32b0b03c2e09401862ff46db94b7925c
Jhon Edison Giraldo <jhonedisongiraldo99@gmail.com>
2019-02-26 17:48:41 -0500

    holi :v

--- client.py
+++ client.py
@@ -3,11 +3,10 @@ import sys
 from selenium import webdriver
 import socket
 
-sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 config=("192.168.0.43",10000)
 
 def send_msg(contacto):
+    sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(config)
     print("conexion establecida")
     sock.send(contacto.encode('utf-8'))
