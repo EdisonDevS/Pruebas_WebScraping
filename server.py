164802bcd695f54a295f9459381c41e74d0fe010
Jhon Edison Giraldo <jhonedisongiraldo99@gmail.com>
2019-02-26 18:34:41 -0500

    full envio

--- server.py
+++ server.py
@@ -2,13 +2,14 @@
 import sys
 from selenium import webdriver
 import socket
+from time import sleep
 
 sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 print("Iniciando servidor")
 config=("192.168.0.43",10000)
 sock.bind(config)
-sock.listen(100)
+sock.listen(1)
 
 print("Servidor en pie")
 
@@ -18,22 +19,25 @@ driver.get("https://web.whatsapp.com/")
 while True:
     con, direccion = sock.accept()
     print("coneccion desde: "+direccion[0])
-    contacto=con.recv(4000)
+    while True:
+        mensaje=con.recv(4000)
+        if mensaje:
+            mensaje=mensaje.decode('utf-8')
+            mensaje_descompuesto=mensaje.split('€')
+            contacto=mensaje_descompuesto[0]
+            msg=mensaje_descompuesto[1]
 
-    if contacto:
-        while True:
-            msg=con.recv(4000)
-            if msg:
-                print("Leegó: "+msg.decode('utf-8'))
-                try:
-                    usuario=driver.find_element_by_xpath('//span[@title="{}"]'.format(contacto.decode('utf-8')))
-                    usuario.click()
-                except:
-                    print("No se ha encontrado el contacto")
+            print("Contacto: "+contacto)
+            print("Mensaje: "+msg)
+            try:
+                usuario=driver.find_element_by_xpath('//span[@title="{}"]'.format(contacto))
+                usuario.click()
+            except:
+                print("No se ha encontrado el contacto")
 
-                caja=driver.find_element_by_class_name("_2S1VP")
-                caja.send_keys(msg.decode('utf-8'))
+            caja=driver.find_element_by_class_name("_2S1VP")
+            caja.send_keys(msg)
 
-                enviar=driver.find_element_by_class_name("_35EW6")
-                enviar.click()
-                break
+            enviar=driver.find_element_by_class_name("_35EW6")
+            enviar.click()
+            break
