bbbf36ddcf81232937f7a8fd28cbf8fea767740b
Jhon Edison Giraldo <jhonedisongiraldo99@gmail.com>
2019-03-05 18:11:32 -0500

    una ui medio funcional

---
+++ QT/HiveClient.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+import sys
+from PyQt5 import QtGui, QtCore, uic, QtWidgets
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+import socket
+
+config=("localhost",10000)
+
+qtCreatorFile = "Hive.ui" 
+
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.actual_contact=""
+        self.setupUi(self)
+        self.ftc=False
+        self.btnEnviar.clicked.connect(self.first_time_connection)
+
+    def first_time_connection(self):
+        if self.ftc:
+            self.send_msg()
+        else:
+            self.request_contacts()
+
+    def request_contacts(self):
+        sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(config)
+        print("conexion establecida")
+        msg="ftc_request€contacts"
+
+        sock.send(msg.encode('utf-8'))
+                
+        c=0
+        #solicito la cantidad de contactos
+        while(True):
+            cant_contacts=sock.recv(10)
+            if(cant_contacts):
+                c=int(cant_contacts.decode('utf-8'))
+                break
+        #solicito todos los contactos
+        self.contacts=[]
+        for x in range(c):
+            contacto=sock.recv(1000)
+            button = QtWidgets.QPushButton(contacto.decode('utf-8'))
+            button.clicked.connect(self.setContact)
+            self.contacts.append(button)
+
+        for x in self.contacts:
+            self.layCont.addWidget(x)
+
+        sock.close()
+        self.ftc=True
+
+    def setContact(self):
+        for x in self.contacts:
+            if x is self.sender():
+                self.actual_contact=x.text()
+                self.lblChat.setText("Chat: "+x.text())
+
+    def send_msg(self):
+        msg=self.txtMsg.toPlainText()
+        sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(config)
+        print("conexion establecida")
+        msg=self.actual_contact+"€"+msg
+        sock.send(msg.encode('utf-8'))
+        sock.close()
+        self.txtMsg.clear()
+
+
+if __name__ == "__main__":
+    app =  QtWidgets.QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
---
+++ QT/HiveServer.py
@@ -0,0 +1,60 @@
+#-*- coding: utf-8 -*-
+import sys
+from selenium import webdriver
+import socket
+from time import sleep
+
+sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print("Iniciando servidor")
+config=("localhost",10000)
+sock.bind(config)
+sock.listen(1)
+
+print("Servidor en pie")
+
+driver=webdriver.Chrome("/home/edison/chromedriver")
+driver.get("https://web.whatsapp.com/")
+
+
+
+while True:
+    con, direccion = sock.accept()
+    print("coneccion desde: "+direccion[0])
+    while True:
+        mensaje=con.recv(4000)
+        if mensaje:
+            mensaje=mensaje.decode('utf-8')
+            mensaje_descompuesto=mensaje.split('€')
+            contacto=mensaje_descompuesto[0]
+            msg=mensaje_descompuesto[1]
+            
+            if contacto=="ftc_request":
+                #se solicitan los contactos para la primera conección
+
+                chats = driver.find_elements_by_xpath('//span[@class = "_1wjpf"]')
+
+                cant_contacts=str(len(chats))
+                
+                con.send(cant_contacts.encode('utf-8'))
+
+                for contacto in chats:
+                    nombre = contacto.get_attribute("title")
+                    print(nombre)
+                    con.send(nombre.encode('utf-8'))
+            else:
+                print("Contacto: "+contacto)
+                print("Mensaje: "+msg)
+                try:
+                    usuario=driver.find_element_by_xpath('//span[@title="{}"]'.format(contacto))
+                    usuario.click()
+                except:
+                    print("No se ha encontrado el contacto")
+
+                caja=driver.find_element_by_class_name("_2S1VP")
+                caja.send_keys(msg)
+
+                enviar=driver.find_element_by_class_name("_35EW6")
+                enviar.click()
+            
+            break
