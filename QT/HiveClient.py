89ce0b48c440002f0be0ebca1240470fe7df3180
Jhon Edison Giraldo <jhonedisongiraldo99@gmail.com>
2019-03-10 22:44:15 -0500

    Recibe y envia mensajes

--- QT/HiveClient.py
+++ QT/HiveClient.py
@@ -56,16 +56,81 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
                 self.actual_contact=x.text()
                 self.lblChat.setText("Chat: "+x.text())
 
+        for i in reversed(range(self.layIn.count())): 
+            self.layIn.itemAt(i).widget().deleteLater()
+            self.layOut.itemAt(i).widget().deleteLater()
+
+        print("actualizando")
+        sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(config)
+        actualizar=self.actual_contact+"€#$%"
+        sock.send(actualizar.encode('utf-8'))
+           
+        while True:
+            paquete=sock.recv(4000) 
+            #self.Ordenar(historial)
+            if paquete:
+                sock.close()
+                paquete=paquete.decode('utf-8')
+                print (paquete)
+                self.Ordenar(paquete)
+                break
+
+
+    def Ordenar(self, paquete):
+        enviadosString=paquete.split("<=>")[0]
+        recibidosString=paquete.split("<=>")[1]
+        todosString=paquete.split("<=>")[2]
+
+        enviados=enviadosString.split("€")
+        recibidos=recibidosString.split("€")
+        todos=todosString.split("€")
+
+        todos.remove(todos[0])
+        enviados.remove(enviados[0])
+        recibidos.remove(recibidos[0])
+
+        send_buttons=[]
+        recived_buttons=[]
+        while todos!=[]:
+            if enviados!=[] and todos[0]==enviados[0]:
+                #print("Yo: "+enviados[0])
+                button = QtWidgets.QPushButton(enviados[0])
+                button.setStyleSheet("border: 0px solid rgb(224, 161, 75); background-color: rgb(224, 161, 75)")
+                send_buttons.append(button)
+                void_button = QtWidgets.QPushButton("")
+                void_button.setStyleSheet("border: 0px solid white;") 
+                recived_buttons.append(void_button)
+                todos.remove(todos[0])
+                enviados.remove(enviados[0])
+            elif recibidos!=[] and todos[0]==recibidos[0]:
+                #print("Otro: "+recibidos[0])
+                void_button = QtWidgets.QPushButton("")
+                void_button.setStyleSheet("border: 0px solid white;")
+                send_buttons.append(void_button)
+                button = QtWidgets.QPushButton(recibidos[0])
+                button.setStyleSheet("border: 0px solid rgb(232, 225, 71);background-color: rgb(232, 225, 71)")
+                recived_buttons.append(button)
+                todos.remove(todos[0])
+                recibidos.remove(recibidos[0])
+            else:
+                todos.remove(todos[0])
+        
+        for btn in send_buttons:
+            self.layOut.addWidget(btn)
+        
+        for btn in recived_buttons:
+            self.layIn.addWidget(btn)
+
     def send_msg(self):
         msg=self.txtMsg.toPlainText()
         sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect(config)
-        print("conexion establecida")
+        print("conectado")
         msg=self.actual_contact+"€"+msg
         sock.send(msg.encode('utf-8'))
         sock.close()
         self.txtMsg.clear()
-
 
 if __name__ == "__main__":
     app =  QtWidgets.QApplication(sys.argv)
--- QT/HiveServer.py
+++ QT/HiveServer.py
@@ -19,7 +19,7 @@ driver.get("https://web.whatsapp.com/")
 
 while True:
     con, direccion = sock.accept()
-    print("coneccion desde: "+direccion[0])
+    print("conexion desde: "+direccion[0])
     while True:
         mensaje=con.recv(4000)
         if mensaje:
@@ -39,20 +39,57 @@ while True:
                     contacts = contacts + "€" + nombre
 
                 print(contacts)
-                con.send(contacts.encode('utf-8'))    
+                con.send(contacts.encode('utf-8'))
+            elif msg=="#$%":
+                try:
+                    paquete=""
+                    #En el navegador se busca el span cuya propiedad de titulo en HTML es igual al nombre del usuario
+                    user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(contacto))
+                    user.click()
+
+                    sleep(2)
+
+                    #se ubican los mensajes de salida por su xpath
+                    salida=driver.find_elements_by_xpath('//div[@class = "_3_7SH _3DFk6 message-out" or @class = "_3_7SH _3DFk6 message-out tail"]/div[@class="Tkt2p"]/div[@class="copyable-text"]/div[@class="_3zb-j ZhF0n"]/span[@class="selectable-text invisible-space copyable-text"]')
+
+                    print("\nMENSAJES ENVIADOS:")
+                    for x in salida:
+                        print (x.get_attribute('innerHTML'))
+                        paquete=paquete+"€"+x.get_attribute('innerHTML')
+
+                    paquete=paquete+"<=>"
+                    #se ubican los mensajes de entrada por su xpath
+                    entrada=driver.find_elements_by_xpath('//div[@class = "_3_7SH _3DFk6 message-in" or @class = "_3_7SH _3DFk6 message-in tail"]/div[@class="Tkt2p"]/div[@class="copyable-text"]/div[@class="_3zb-j ZhF0n"]/span[@class="selectable-text invisible-space copyable-text"]')
+
+                    print("\nMENSAJES RECIBIDOS:")
+                    for x in entrada:
+                        print (x.get_attribute('innerHTML'))
+                        paquete=paquete+"€"+x.get_attribute('innerHTML')
+
+                    paquete=paquete+"<=>"                                                                       
+                    todos=driver.find_elements_by_xpath('//span[@class="selectable-text invisible-space copyable-text"]')
+
+                    print("\nTODOS LOS MENSAJES:")
+                    for x in todos:
+                        print (x.get_attribute('innerHTML'))
+                        paquete=paquete+"€"+x.get_attribute('innerHTML')
+
+                    con.send(paquete.encode('utf-8'))
+                    print("se envió")
+                except:
+                    print("No se ha encontrado el contacto")
             else:
                 print("Contacto: "+contacto)
                 print("Mensaje: "+msg)
                 try:
                     usuario=driver.find_element_by_xpath('//span[@title="{}"]'.format(contacto))
                     usuario.click()
+                    caja=driver.find_element_by_class_name("_2S1VP")
+                    caja.send_keys(msg)
+
+                    enviar=driver.find_element_by_class_name("_35EW6")
+                    enviar.click()
                 except:
                     print("No se ha encontrado el contacto")
-
-                caja=driver.find_element_by_class_name("_2S1VP")
-                caja.send_keys(msg)
-
-                enviar=driver.find_element_by_class_name("_35EW6")
-                enviar.click()
             
             break
